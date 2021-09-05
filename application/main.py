4bde4df774dda00a5413318ba690853913752ac3
Prabhat14176 <prabhat.shukla@mastek.com>
2021-09-05 16:04:53 +0530

    Changes

--- application/main.py
+++ application/main.py
@@ -19,19 +19,19 @@ class Application():
         input = Label(ws, text='Upload first Input file in xlsx format')
         input.grid(row=0, column=0, padx=10)
 
-        inputbtn = Button(ws, text ='Choose File', command = lambda:self.up.open_file('i')) 
+        inputbtn = Button(ws, text ='Choose File', command = lambda:self.up.open_file('i',self.show_label_for_file_upload)) 
         inputbtn.grid(row=0, column=1)
 
         output = Label(ws, text='Upload secomd Input file in xlsx format')
         output.grid(row=1, column=0, padx=10)
 
-        outputbtn = Button(ws, text ='Choose File ', command = lambda:self.up.open_file('o')) 
+        outputbtn = Button(ws, text ='Choose File ', command = lambda:self.up.open_file('o',self.show_label_for_file_upload)) 
         outputbtn.grid(row=1, column=1)
 
         filter = Label(ws,text='Upload filter data excel in xlsx format ')
         filter.grid(row=2, column=0, padx=10)
 
-        filterbtn = Button(ws, text ='Choose File', command = lambda:self.up.open_file('f')) 
+        filterbtn = Button(ws, text ='Choose File', command = lambda:self.up.open_file('f',self.show_label_for_file_upload)) 
         filterbtn.grid(row=2, column=1)
 
         upld = Button(ws, text='Upload Files', command=self.uploadFiles)
@@ -54,12 +54,18 @@ class Application():
                 pb1['value'] += 20
                 time.sleep(1)
             pb1.destroy()
-            Label(self.ws, text="Excel Copied Successfully!", foreground="green").grid(row=4, columnspan=3, pady=10)
+            self.show_label( text="Excel Copied Successfully!", color="green")
         else:
-            Label(self.ws, text="Please upload a valid file", foreground="red").grid(row=4, columnspan=3, pady=10)
+            self.show_label( text="Please upload a valid file", color="red")
+
+    def show_label_for_file_upload(self,text,color):
+        lbl1=Label(self.ws, text=text, foreground=color)
+        lbl1.grid(row=4, columnspan=3, pady=5)
+
 
     def show_label(self,text,color):
-        Label(self.ws, text=text, foreground=color).grid(row=4, columnspan=3, pady=10)
+        self.show_label_for_file_upload("                                        ","white")
+        Label(self.ws, text=text, foreground=color).grid(row=6, columnspan=3, pady=10)
 
 ws = Tk()
 app = Application(ws)
--- application/upload.py
+++ application/upload.py
@@ -13,15 +13,37 @@ class Upload():
         self.input2_file =  StringVar()
         self.filter_file =  StringVar()
     
-    def open_file(self, file):
+    def getuploadedstatusmsg(self):
+        msg=""
+        if (self.hasValue(self.input1_file)):
+            msg="File 1 has been uploaded"
+        if (self.hasValue(self.input2_file)):
+            msg="File 2 has been uploaded"
+        if (self.hasValue(self.filter_file)):
+            msg="File 3 has been uploaded"
+        if (self.hasValue(self.input1_file) and self.hasValue(self.input2_file)):
+            msg="File 1 and File 2 have been uploaded"
+        if (self.hasValue(self.input1_file) and self.hasValue(self.filter_file)):
+            msg="File 1 and File 3 have been uploaded"
+        if (self.hasValue(self.input2_file) and self.hasValue(self.filter_file)):
+            msg="File 2 and File 3 have been uploaded"
+        if (self.hasValue(self.input1_file) and self.hasValue(self.input2_file) and self.hasValue(self.filter_file)):
+            msg="All the three files have been uploaded"
+        return msg
+
+
+    def open_file(self, file, show_label):
         file_path = askopenfilename(initialdir = "/", title = "Select a File", filetypes=[('Excel Files', '*xlsx'),("all files")])
         if file_path is not None:
             if file == 'i':
                 self.input1_file.set(file_path)
+                show_label(self.getuploadedstatusmsg(),"green")
             elif file == 'o':
                 self.input2_file.set(file_path)
+                show_label(self.getuploadedstatusmsg(),"green")
             elif file == 'f':
                 self.filter_file.set(file_path)
+                show_label(self.getuploadedstatusmsg(),"green")
             else:
                 raise("Something went wrong")
         else:
